d7a4e91ce6ba48393b8ac7781043181029d61b4c
VictorMarques <jsphmarques@gmail.com>
2026-07-24 10:49:54 -0300

    Create first SpringBoot project

---
+++ src/test/java/io/github/victormqs/textextractor/TextExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.victormqs.textextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
