27adfcb9a465f1d27ff4b2b4ac49bdb4354c765a
VictorMarques <jsphmarques@gmail.com>
2026-07-27 23:15:44 -0300

    Add HTML upload form and implement file upload endpoint

--- src/main/java/io/github/victormqs/textextractor/controller/FileUploadController.java
+++ src/main/java/io/github/victormqs/textextractor/controller/FileUploadController.java
@@ -1,8 +1,22 @@
 package io.github.victormqs.textextractor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileUploadController {
+    @GetMapping("/")
+    public String index() {
+        return "uploadForm";
+    }
+
+    @PostMapping("/upload")
+    public String upload (@RequestParam("file")MultipartFile file) {
+        System.out.println(file.getOriginalFilename());
+        return "uploadForm";
+    }
 
 }
